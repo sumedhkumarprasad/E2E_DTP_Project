a7bb5f14efdc496f88ea2c811862def458dff494
sumedhkumarprasad <sumedhkumarprasad@gmail.com>
2023-04-22 12:06:26 +0530

    created functions for databases

--- src/utils.py
+++ src/utils.py
@@ -26,7 +26,7 @@ logging.basicConfig(filename='myapp.log',
                     format='%(asctime)s %(message)s', 
                     level=logging.INFO)
 
-env = Path(".env.txt")
+env = Path("src/.env")
 load_dotenv(dotenv_path=env)
 
 # STEP 1
@@ -51,22 +51,81 @@ def connect_db(host: str, user: str) -> Tuple[mysql.connection.MySQLConnection, 
     except Exception as e:
         logging.debug(e)
     return conn, cur
-
+    
 conn, cur = connect_db(host="localhost", user="root")
+conn.close()
+cur.close()
 
 
 
-# STEP 2 CREATE A FUNCTION: GET_DATA OR READ_DATA
-sample_data_df = pd.read_excel( 'sample_data_mysql.xlsx', sheet_name= "sample_data" )
-sample_data_df.head(5)
 
-# STEP 3 CREATE FUNCTION CREATE DATABASE - RETURNS DATABASES
-cur.execute("CREATE DATABASE sample_data_upload")
-cur.execute("SHOW DATABASES")
-databases = cur.fetchall()
 
-# STEP 4
-def python_df_to_sql_table(dataframe):
+# # STEP 2 CREATE A FUNCTION: GET_DATA OR READ_DATA
+# Filename argurment
+def read_data(filename: str) -> pd.DataFrame:
+    '''
+    Reads a CSV file and returns a pandas DataFrame.
+
+    Parameters
+    ----------
+    filename : str
+    - file_path (str): The path to the CSV file.   
+
+    Returns
+    -------
+    df : TYPE
+        DESCRIPTION.
+    - pandas.DataFrame: A DataFrame containing the data from the CSV file.
+
+    '''
+    df = pd.read_csv(filename)
+    return df
+
+
+# # STEP 3 CREATE FUNCTION CREATE DATABASE - RETURNS DATABASES
+# database name 
+def create_database(database_name: str): 
+    '''
+    This function will create the database and will return list of all databases
+    
+    Parameters
+    ----------
+    database_name : str
+        Pass the Database name which we want to create it
+
+    Returns
+    -------
+    databases : TYPE
+        DESCRIPTION.
+
+    '''
+    conn, cur = connect_db(host="localhost", user="root")
+    cur.execute(f"CREATE DATABASE {database_name}")
+    cur.execute("SHOW DATABASES")
+    databases = cur.fetchall()
+    return databases
+
+# # STEP 4
+def python_df_to_sql_table(dataframe: str) ->  pd.DataFrame :
+    '''
+    This function will used to create the  table structure from Python dataframe to SQL table
+    coltype will return 'colname datatype'
+    values will return the place holder to inster the data into it and it will have number of count will 
+    equal to no of columns
+    
+    Parameters
+    ----------
+    dataframe : str
+        DESCRIPTION.
+
+    Returns
+    -------
+    coltypes : TYPE
+        output will be one string with colname and dattype 'colA INT, colb FLOAT'
+    values : TYPE
+        Placeholder return. No of columns will equal no of place holder 
+
+    '''
     types =[]
     for type in dataframe.dtypes:
         if type == "object":
@@ -82,29 +141,70 @@ def python_df_to_sql_table(dataframe):
     values = ', '.join(["%s" for i in range(len(dataframe.columns))])
     return coltypes, values
 
-coltype, values = python_df_to_sql_table(sample_data_df)
+coltype, values = python_df_to_sql_table(dataframe)
 
-# STEP 5 CREATE FUNCTION CREATE_TABLE - NO RETURN
-cur.execute("use sample_data_upload")
-cur.execute("CREATE TABLE sample_data_table (sl_no int,cust_id  VARCHAR(255) PRIMARY KEY, region VARCHAR(255), date DATE)")
 
+# # STEP 5 CREATE FUNCTION CREATE_TABLE - NO RETURN
+def create_table_in_sql(database_name: str, table_name: str, dataframe: str):
+    '''
+    This function will create the datbase
+     
+    Parameters
+    ----------
+    database_name : str
+        Pass the database name which is already created here new database will not get create
+    table_name : str
+       Pass the table name which we need to create the table inside the database
+
+    Returns
+    -------
+    It will create table inside the database
+
+    '''
+    conn, cur = connect_db(host="localhost", user="root")
+    
+    coltype, values = python_df_to_sql_table(dataframe)
+    
+    cur.execute(f"use {database_name}")
+    cur.execute(f"CREATE TABLE {table_name} ({col_type}")
+    cur.execute("SHOW TABLES")
+    tables = cur.fetchall()
 
 
    
 
 
-# for step 6
+# Step 6
 
-for i, row in sample_data_df.iterrows():    
-    sql = f"INSERT INTO sample_data_table VALUES ({values})"
-    cur.execute(sql, tuple(row))
-    conn.commit()
-cur.close()
-conn.close()
+def python_to_sql_data_transfer_func(dataframe: str, table_name: str):
+    '''
+    Passing  the data from python datframe to sql table
+
+    Parameters
+    ----------
+    dataframe : str
+        passing the python dataframe which is read from the system 
+    table_name : str
+        where we need to store the python dataframe data to sql table
+
+    Returns
+    -------
+    None.
+
+    '''
+    conn, cur = connect_db(host="localhost", user="root")
+    coltype, values = python_df_to_sql_table(dataframe)
     
-cur.execute("SELECT * FROM sample_data_table")
-myresult = cur.fetchall()
-myresult 
-
-for x in myresult:
-  print(x)
+    for i, row in dataframe.iterrows():    
+        sql = f"INSERT INTO {table_name} VALUES ({values})"
+        cur.execute(sql, tuple(row))
+        conn.commit()
+    cur.close()
+    conn.close()
+        
+    cur.execute(f"SELECT * FROM {table_name}")
+    myresult = cur.fetchall()
+    myresult 
+    
+    for x in myresult:
+      print(x)
